540d2db7584c31cbfe55f9acceb6f1868bc45c90
Habeeb20 <Ademolawaliyu1@gmail.com>
2025-08-05 20:08:11 +0100

    commit

--- EventPlanner/app/planners.jsx
+++ EventPlanner/app/planners.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text,ScrollView, Pressable } from 'react-native';
-
+import FooterNav from './FooterNav';
 
 export default function planners() {
   return (
-    <View>
+    <>
+      <View>
       <Text>hi planners</Text>
      </View>
+     <FooterNav />
+
+    </>
+  
   );
 }
